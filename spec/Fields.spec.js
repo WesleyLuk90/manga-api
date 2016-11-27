873904b355c0f330e6ab678b9fbd2978957eb071
Wesley Luk <wesleyluk@gmail.com>
2016-11-26 21:30:34 -0800

    Fixed test

--- spec/Fields.spec.js
+++ spec/Fields.spec.js
@@ -3,6 +3,6 @@ const Fields = require('../sdk/Fields');
 describe('Fields', () => {
     it('should check for valid fields', () => {
         expect(Fields.validField({})).toBe(false);
-        expect(Fields.validField(Fields.AUTHOR)).toBe(false);
+        expect(Fields.validField(Fields.AUTHOR)).toBe(true);
     });
 });
