05419a2831e7366da22a4c86973fd31234db4fcb
Wesley Luk <wesleyluk@gmail.com>
2016-12-01 21:35:20 -0800

    Export other classes

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const RepositoryListFactory = require('./repositories/RepositoryListFactory');
 const MangaHandle = require('./sdk/MangaHandle');
 const Manga = require('./sdk/Manga');
+const Chapter = require('./sdk/Chapter');
+const ChapterHandle = require('./sdk/ChapterHandle');
+const Page = require('./sdk/Page');
+const PageHandle = require('./sdk/PageHandle');
 
 module.exports = {
     RepositoryListFactory,
     Manga,
     MangaHandle,
+    Chapter,
+    ChapterHandle,
+    Page,
+    PageHandle,
 };
