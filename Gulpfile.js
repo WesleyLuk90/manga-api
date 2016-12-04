f267f5a41735089737d9a3de7e668c24da88cc3d
Wesley Luk <wesleyluk@gmail.com>
2016-12-04 11:22:35 -0800

    Made tests fail gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,29 +14,39 @@ const repoSpecs = 'spec/Repositories.spec.js';
 const nonRepoSpecs = [allSpecs, `!${repoSpecs}`];
 const watchFiles = ['sdk/**/*.js', 'repositories/**/*.js', 'spec/**/*.spec.js', 'spec/repository-data/*.data.js'];
 
-gulp.task('test', () => {
-    gulp.src(nonRepoSpecs)
-        .pipe(jasmine(jasmineConfig))
-        .on('error', () => {});
-});
+function catchError(e) {
+    this.emit('end');
+}
 
-gulp.task('test:watch', ['test'], () => {
-    gulp.watch(watchFiles, ['test']);
-});
+gulp.task('test', () =>
+    gulp
+    .src(nonRepoSpecs)
+    .pipe(jasmine(jasmineConfig)));
 
-gulp.task('test-repo', () => {
-    gulp.src(['spec/Repositories.spec.js'])
-        .pipe(jasmine(jasmineConfig))
-        .on('error', () => {});
-});
+gulp.task('test:ignore-errors', () =>
+    gulp
+    .src(nonRepoSpecs)
+    .pipe(jasmine(jasmineConfig))
+    .on('error', catchError));
 
+gulp.task('test:watch', ['test:ignore-errors'], () =>
+    gulp.watch(watchFiles, ['test:ignore-errors']));
 
-gulp.task('test-repo:watch', ['test-repo'], () => {
-    gulp.watch(watchFiles, ['test-repo']);
-});
+gulp.task('test-repo', () =>
+    gulp
+    .src(['spec/Repositories.spec.js'])
+    .pipe(jasmine(jasmineConfig)));
+gulp.task('test-repo:ignore-errors', () =>
+    gulp
+    .src(['spec/Repositories.spec.js'])
+    .pipe(jasmine(jasmineConfig))
+    .on('error', catchError));
 
-gulp.task('default', () => {
-    gulp.src(allSpecs)
-        .pipe(jasmine(jasmineConfig))
-        .on('err', () => {});
-});
+gulp.task('test-repo:watch', ['test-repo:ignore-errors'], () =>
+    gulp.watch(watchFiles, ['test-repo:ignore-errors']));
+
+gulp.task('default', () =>
+    gulp
+    .src(allSpecs)
+    .pipe(jasmine(jasmineConfig))
+    .on('err', catchError));
