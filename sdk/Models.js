00e9c1c314be5986a155331b28cb79e054a2d2f4
Wesley Luk <wesleyluk@gmail.com>
2017-11-18 11:05:21 -0800

    Added array fields

--- sdk/Models.js
+++ sdk/Models.js
@@ -1,42 +1,7 @@
 const assert = require('assert');
 const lodash = require('lodash');
 
-module.exports = class Models {
-    static defineModel(Klass, fields) {
-        assert.equal(typeof Klass, 'function');
-        assert(Array.isArray(fields));
-        fields.forEach((f) => {
-            assert.equal(typeof f, 'string');
-            assert.equal(lodash.camelCase(f), f);
-        });
-
-        fields.forEach((f) => {
-            this.defineDefaultValue(Klass, f);
-            this.defineGetter(Klass, f);
-            this.defineSetter(Klass, f);
-        });
-
-        Klass.create = () => new Klass();
-        Klass.getDefinedFields = () => fields.slice();
-
-        const Builder = class {};
-        Klass.builder = () => new Builder();
-        fields.forEach((f) => {
-            this.defineDefaultValue(Builder, f);
-            this.defineGetter(Builder, f);
-            this.defineNullableSetter(Builder, f);
-        });
-        Builder.prototype.create = function create() {
-            const instance = new Klass();
-            fields.forEach((f) => {
-                if (this[f] != null) {
-                    instance[f] = this[f];
-                }
-            });
-            return instance;
-        };
-    }
-
+class Fields {
     static defineDefaultValue(klass, key) {
         klass.prototype[key] = null;
     }
@@ -62,6 +27,91 @@ module.exports = class Models {
         klass.prototype[setter] = function _setter(value) {
             this[key] = value;
             return this;
+        };
+    }
+}
+
+class ArrayFields {
+    static defineDefaultValue(klass, key) {
+        klass.prototype[key] = [];
+    }
+
+    static defineGetter(klass, key) {
+        const getter = `get${lodash.upperFirst(key)}`;
+        klass.prototype[getter] = function _getter() {
+            return this[key].slice();
+        };
+    }
+
+    static defineSetter(klass, key) {
+        const setter = `set${lodash.upperFirst(key)}`;
+        klass.prototype[setter] = function _setter(value) {
+            assert(Array.isArray(value), `Expected an array but got '${value}'`);
+            this[key] = value.slice();
+            return this;
+        };
+    }
+
+    static defineNullableSetter(klass, key) {
+        const setter = `set${lodash.upperFirst(key)}`;
+        klass.prototype[setter] = function _setter(value) {
+            assert(Array.isArray(value) || value == null, `Expected an array but got '${value}'`);
+            if (!value) {
+                this[key] = [];
+            } else {
+                this[key] = value;
+            }
+            return this;
+        };
+    }
+}
+
+module.exports = class Models {
+    static defineModel(Klass, { fields, arrayFields }) {
+        assert.equal(typeof Klass, 'function');
+        assert(Array.isArray(fields));
+        fields.forEach((f) => {
+            assert.equal(typeof f, 'string');
+            assert.equal(lodash.camelCase(f), f);
+        });
+
+        fields.forEach((f) => {
+            Fields.defineDefaultValue(Klass, f);
+            Fields.defineGetter(Klass, f);
+            Fields.defineSetter(Klass, f);
+        });
+        arrayFields.forEach((f) => {
+            ArrayFields.defineDefaultValue(Klass, f);
+            ArrayFields.defineGetter(Klass, f);
+            ArrayFields.defineSetter(Klass, f);
+        });
+
+        Klass.create = () => new Klass();
+        Klass.getDefinedFields = () => [...fields, ...arrayFields];
+
+        const Builder = class {};
+        Klass.builder = () => new Builder();
+        fields.forEach((f) => {
+            Fields.defineDefaultValue(Builder, f);
+            Fields.defineGetter(Builder, f);
+            Fields.defineNullableSetter(Builder, f);
+        });
+        arrayFields.forEach((f) => {
+            ArrayFields.defineDefaultValue(Builder, f);
+            ArrayFields.defineGetter(Builder, f);
+            ArrayFields.defineNullableSetter(Builder, f);
+        });
+        Builder.prototype.create = function create() {
+            const instance = new Klass();
+            fields.forEach((f) => {
+                if (this[f] != null) {
+                    instance[f] = this[f];
+                }
+            });
+            arrayFields.forEach((f) => {
+                instance[f] = this[f].slice();
+            });
+            return instance;
         };
     }
 };
--- spec/sdk/Models.spec.js
+++ spec/sdk/Models.spec.js
@@ -3,11 +3,16 @@ const Models = require('../../sdk/Models');
 
 describe('Models', () => {
     class SomeModel extends Model {}
-    Models.defineModel(SomeModel, [
-        'a',
-        'b',
-        'anotherField',
-    ]);
+    Models.defineModel(SomeModel, {
+        fields: [
+            'a',
+            'b',
+            'anotherField',
+        ],
+        arrayFields: [
+            'anArray',
+        ],
+    });
 
     it('should extend the base', () => {
         const someModel = new SomeModel();
@@ -24,22 +29,27 @@ describe('Models', () => {
         expect(someModel.getA()).toBe(null);
         expect(someModel.getB()).toBe(null);
         expect(someModel.getAnotherField()).toBe(null);
+        expect(someModel.getAnArray()).toEqual([]);
 
         expect(someModel.a).toBe(null);
         expect(someModel.b).toBe(null);
         expect(someModel.anotherField).toBe(null);
+        expect(someModel.anArray).toEqual([]);
     });
 
     it('should set and get fields', () => {
         const someModel = new SomeModel();
         expect(someModel.setA(10)).toBe(someModel);
+        expect(someModel.setAnArray(['a'])).toBe(someModel);
 
         expect(someModel.getA()).toBe(10);
+        expect(someModel.getAnArray()).toEqual(['a']);
     });
 
     it('should require a value', () => {
         const someModel = new SomeModel();
         expect(() => someModel.setA(null)).toThrowError('Expected a value but got \'null\'');
+        expect(() => someModel.setAnArray(null)).toThrowError('Expected an array but got \'null\'');
     });
 
     describe('builder', () => {
@@ -49,18 +59,24 @@ describe('Models', () => {
             expect(builder.getA()).toBe(null);
             expect(builder.setA(10)).toBe(builder);
             expect(builder.getA()).toBe(10);
+
+            expect(builder.getAnArray()).toEqual([]);
+            expect(builder.setAnArray(['b'])).toBe(builder);
+            expect(builder.getAnArray()).toEqual(['b']);
         });
 
         it('should can set nullable', () => {
             const builder = SomeModel.builder();
             expect(builder.setA(null)).toBe(builder);
             expect(builder.getA()).toBe(null);
+            expect(builder.setAnArray(null)).toBe(builder);
+            expect(builder.getAnArray()).toEqual([]);
         });
 
         it('can create', () => {
             const builder = SomeModel.builder();
-            const model = builder.setA(10).create();
-            expect(model).toEqual(SomeModel.create().setA(10));
+            const model = builder.setA(10).setAnArray(['b']).create();
+            expect(model).toEqual(SomeModel.create().setA(10).setAnArray(['b']));
         });
     });
 });
